831c2201df2766292b89b9bab030228449f960a0
Simon Baier <simonbaier@outlook.de>
2018-06-27 21:30:03 +0200

    cleaner signal processing method using tini as propagation entry point

--- startJD2.sh
+++ startJD2.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-function stopJD2 {
-	PID=$(cat JDownloader.pid)
-	kill $PID
-	wait $PID
-	exit
-}
-
-trap stopJD2 EXIT
-
 echo "{\"devicename\" : \"${JDDEVICENAME}\", \"autoconnectenabledv2\" : true, \"password\" : \"${JDPASSWORD}\", \"email\" : \"${JDEMAIL}\"}" > /opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
 
-java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &
-
-while true; do
-	sleep inf
-done
-
+exec java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar -norestart
